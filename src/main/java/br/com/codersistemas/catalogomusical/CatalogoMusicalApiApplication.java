c666af67cde1c5d0010b263aa642a39cbaf7ba91
Gustavo da Silva <gustavocontabeis@gmail.com>
2020-03-21 16:24:05 -0300

    first commit

---
+++ src/main/java/br/com/codersistemas/catalogomusical/CatalogoMusicalApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.codersistemas.catalogomusical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoMusicalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoMusicalApiApplication.class, args);
+	}
+
+}
