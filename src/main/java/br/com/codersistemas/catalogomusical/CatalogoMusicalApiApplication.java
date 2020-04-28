be430d4ecf2b7f257966e7282574117bac119443
Gustavo da Silva <gustavocontabeis@gmail.com>
2020-04-27 21:41:22 -0300

    first commit

--- src/main/java/br/com/codersistemas/catalogomusical/CatalogoMusicalApiApplication.java
+++ src/main/java/br/com/codersistemas/catalogomusical/CatalogoMusicalApiApplication.java
@@ -2,13 +2,14 @@ package br.com.codersistemas.catalogomusical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 //@EnableJpaRepositories("br.com.codersistemas.catalogomusical")
 //@ComponentScan ({"br.com.codersistemas.catalogomusical.repository"})
-public class CatalogoMusicalApiApplication {
+public class CatalogoMusicalApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoMusicalApiApplication.class, args);
